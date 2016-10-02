361b6efe4abc909a35616c3d3efe38d6f3bc8be7
CoDesign333-4 <faisalhmohd@live.com>
2016-10-02 17:25:38 +0600

    Initial files

---
+++ app.js
@@ -0,0 +1,23 @@
+'use strict'
+
+let exec = require('child_process').exec,
+    cmd = 'arp -lna',
+    result = [];
+
+exec(cmd, function(error, stdout, stderr) {
+    let data = stdout.split("\n");
+    for (var i = 1; i <= data.length; i++) {
+        if (i == data.length) {
+            console.log(result);
+        } else {
+            let deviceInfo = data[i].split(" ");
+            // console.log(deviceInfo);
+            result.push({
+                ip: deviceInfo[0],
+                macaddress: (deviceInfo[11]) ?  deviceInfo[11] : deviceInfo[12]
+            })
+        }
+
+    }
+
+});
